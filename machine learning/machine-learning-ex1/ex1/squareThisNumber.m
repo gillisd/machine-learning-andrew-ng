e88e32a62734c8b4ef6e939e3bf522b43e5c92c2
David Gillis <david@gratr.io>
2017-04-15 16:05:23 -0700

    commit completed assignments for weeks 1-5

---
+++ machine learning/machine-learning-ex1/ex1/squareThisNumber.m
@@ -0,0 +1,7 @@
+function [ output_args ] = squareThisNumber( input_args )
+%SQUARETHISNUMBER Summary of this function goes here
+%   Detailed explanation goes here
+
+output_args = input_args ^ 2;
+end
+
---
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -0,0 +1,28 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid function
+%   g = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+
+m = size(z, 1);
+n = size(z, 2);
+for i = 1:m
+    for j = 1:n
+    g(i,j) = 1 / (1 + exp(-z(i,j)));
+    end;
+end;
+
+
+    
+
+
+
+% =============================================================
+
+end
